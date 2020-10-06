bc8b46a391c09954bc05b738fdcadd6affc4962e
Funakoshi Silva <55293531+funakoshiapi@users.noreply.github.com>
2020-10-06 18:34:24 -0400

    Create polygon.py

---
+++ Polygon/polygon.py
@@ -0,0 +1,40 @@
+import turtle
+
+class Polygon:
+    def __init__(self, sides, name, size=100, color="black",line_thickness= 3):
+        self.sides = sides
+        self.name = name
+        self.size = size
+        self.color = color
+        self.line_thickness = line_thickness
+        self.interior_angles = (self.sides-2)*180
+        self.angle = self.interior_angles/self.sides
+    
+    def draw(self):
+        turtle.color(self.color)
+        turtle.pensize(self.line_thickness)
+        for i in range(self.sides):
+            turtle.forward(self.size)
+            turtle.right(180-self.angle)
+
+# inherites from Polygon           
+class Square(Polygon):
+    def __init__(self, size=100, color="black",line_thickness=3):
+      #sub classing
+      super().__init__(4, "Square", size, color,line_thickness)
+
+    def draw(self):
+      turtle.begin_fill()
+      super().draw()
+      turtle.end_fill()
+
+
+def main():
+
+  square = Square(color = "#123abc", size=100)
+  square.draw()
+  turtle.done()
+
+if __name__ == "__main__":
+  main()
+      
